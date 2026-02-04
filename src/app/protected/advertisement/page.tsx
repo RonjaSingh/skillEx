a5846605b012ca625be6fbc1cdc84f20f7b69086
RonjaSingh <ronja-maria.singh@dci-student.org>
2026-02-04 09:55:58 +0100

    correct foldernames & default function names

--- src/app/protected/advertisement/page.tsx
+++ src/app/protected/advertisement/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
+export default function AdvertisementPage() {
   return <div>Meine Anzeigen</div>
 }
---
+++ src/app/protected/sessions/page.tsx
@@ -0,0 +1,3 @@
+export default function SessionPage() {
+  return <div>My Sessions</div>
+}
