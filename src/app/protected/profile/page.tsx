7163a1fcfd122b925f606e15fa3b778eec285b4a
RonjaSingh <ronja-maria.singh@dci-student.org>
2026-02-04 11:34:45 +0100

    feat(profile):init profile feature

--- src/app/protected/profile/page.tsx
+++ src/app/protected/profile/page.tsx
@@ -1,4 +1,32 @@
 
+
+'use client'
+
+import { useRouter } from 'next/navigation'
+
 export default function ProfilePage() {
-  return <div>My profile</div>
+  const router = useRouter()
+  return (
+    <div>
+      <h1>Mein Profil</h1>
+
+      <p>
+        <strong>Name:</strong> Max Mustermann
+      </p>
+
+      <p>
+        <strong>Skills:</strong> JavaScript, React
+      </p>
+
+      <p>
+        <strong>Sprachen:</strong> Englisch
+      </p>
+
+      <button>Profil bearbeiten</button>
+
+      <button onClick={() => router.push('/protected/sessions')}>
+        Meine Sessions
+      </button>
+    </div>
+  )
 }
