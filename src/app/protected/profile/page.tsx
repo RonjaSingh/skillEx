c777eb1e3b99a0f379fbb2672afce300c5c0db72
RonjaSingh <ronja-maria.singh@dci-student.org>
2026-02-04 13:20:53 +0100

    feat(profile): add editprofile-popup

--- src/app/protected/profile/page.tsx
+++ src/app/protected/profile/page.tsx
@@ -1,38 +1,52 @@
 'use client'
 
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import EditProfilePopup from '@/components/editprofile-popup'
 
 export default function ProfilePage() {
   const router = useRouter()
 
+  const [name, setName] = useState('')
+  const [skills, setSkills] = useState('')
+  const [language, setLanguage] = useState('')
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+
+
   return (
     <div className="max-w-xl mx-auto mt-10 p-4 space-y-6">
-    
+
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Profile:</h1>
-        <button className="px-4 py-2 border rounded hover:bg-gray-100 transition">
+
+        <h1 className="text-2xl font-bold">Profile</h1>
+        {/* profil bearbeiten button */}
+        <button className="px-4 py-2 border rounded hover:bg-gray-100 transition"
+          onClick={() => setIsPopupOpen(true)}>
           Edit Profile
         </button>
       </div>
 
       <div className="space-y-2">
         <p>
-          <strong>Name:</strong> 
+          <strong>Name:</strong>{name}
         </p>
 
         <p>
-          <strong>Skills:</strong> 
+          <strong>Skills:</strong>{skills}
         </p>
 
         <p>
-          <strong>Language:</strong> 
+          <strong>Language:</strong>{language}
         </p>
 
-   
+
         <div className="flex justify-between items-center mt-2">
           <p>
             <strong>Attended Sessions:</strong>
           </p>
+
+          {/* session uebersicht button */}
           <button
             onClick={() => router.push('/protected/sessions')}
             className="px-4 py-2 border rounded hover:bg-gray-100 transition"
@@ -40,6 +54,17 @@ export default function ProfilePage() {
             My Sessions
           </button>
         </div>
+        {isPopupOpen && (
+          <EditProfilePopup
+            name={name}
+            setName={setName}
+            skills={skills}
+            setSkills={setSkills}
+            language={language}
+            setLanguage={setLanguage}
+            onClose={() => setIsPopupOpen(false)}
+          />
+        )}
       </div>
     </div>
   )
