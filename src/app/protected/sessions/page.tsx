c44ed719984ced64bafc13a21be873a58ed56947
Raphael <raphael.lesmann@dci-student.org>
2026-02-05 18:26:46 +0100

    data base access

--- src/app/protected/sessions/page.tsx
+++ src/app/protected/sessions/page.tsx
@@ -22,4 +22,27 @@ export default function SessionsPage() {
     loadSessions()
   }, [])
 
-  
+    async function loadSessions() {
+    const now = new Date().toISOString()
+
+    const { data } = await supabase
+      .from('session_request')
+      .select(`
+        session_request_id,
+        status,
+        availability(start_time),
+        advertisement(title),
+        user!session_request_request_from_user_id_fkey(name)
+      `)
+
+    if (!data) return
+
+    const mapped = data.map((s: any) => ({
+      id: s.session_request_id,
+      start_time: s.availability?.start_time,
+      title: s.advertisement?.title,
+      name: s.user?.name,
+      status: s.status
+    }))
+
+   
