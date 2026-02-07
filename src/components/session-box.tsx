3fd5078d9935c0226859646bd83c37e7eca838c1
Raphael <raphael.lesmann@dci-student.org>
2026-02-07 19:07:51 +0100

    default function session box, props and info logic

--- src/components/session-box.tsx
+++ src/components/session-box.tsx
@@ -15,3 +15,36 @@ type Props = {
   onReject?: (id: string) => void
   onCancel?: (id: string) => void
 }
+
+export default function SessionBox({
+  title,
+  data,
+  showAcceptReject,
+  showCancel,
+  showRate,
+  onAccept,
+  onReject,
+  onCancel
+}: Props) {
+  return (
+    <div >
+
+      <h2 >{title}</h2>
+
+      {data.map(session => (
+        <div
+          key={session.id}
+          
+        >
+          <div>{new Date(session.start_time).toLocaleString()}</div>
+          <div>{session.title}</div>
+          <div>{session.name}</div>
+
+          <div >
+            
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
