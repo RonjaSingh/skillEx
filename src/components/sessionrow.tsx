c8c9cfc82d50f8016a6eaf3a8c159bd0b526e184
Raphael <raphael.lesmann@dci-student.org>
2026-02-12 11:55:15 +0100

    add props, usestates, handleSave function

--- src/components/sessionrow.tsx
+++ src/components/sessionrow.tsx
@@ -1,22 +1,54 @@
+'use client'
+import { useState } from 'react'
+
+
+
 type Props = {
   date: string
   topic: string
   name: string
   description?: string
   editable?: boolean
-  onEdit?: () => void
+  onSave?: (newDescription: string) => void
   actions?: React.ReactNode
 }
 
-export default function SessionRow({ date, topic, name, description,
-  editable, onEdit, actions }: Props) {
-  return (
+export default function SessionRow({
+  date,
+  topic,
+  name,
+  description,
+  editable,
+  onSave,
+  actions
+}: Props)  {
+  
+    const [expanded, setExpanded] = useState(false)
+  const [editing, setEditing] = useState(false)
+  const [text, setText] = useState(description || '')
+
+  function handleSave() {
+    onSave?.(text)
+    setEditing(false)
+  }
+  
+  
+  
+  
+    return (
+
+
     <div className="grid grid-cols-5 items-center border p-2 rounded text-sm">
       <div>{date}</div>
       <div>{topic}</div>
       <div>{name}</div>
-      <div className="text-gray-600 truncate">
-        {description || '-'}
+
+
+        <div
+          className="text-gray-600 truncate cursor-pointer"
+          onClick={() => setExpanded(!expanded)}
+        >
+         {description || '-'}
       </div>
 
       <div className="flex gap-2 justify-end">
