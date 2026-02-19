49f586b85f878d992391da2a8938c114c63d442a
RonjaSingh <ronja-maria.singh@dci-student.org>
2026-02-19 09:11:54 +0100

    feat(header): add Logo in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-  
+
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -14,14 +14,16 @@ export function Header({ username, avatarUrl }: HeaderProps) {
       <div className="max-w-6xl mx-auto px-4 py-2 flex items-center justify-between">
 
         {/* Left: Logo */}
-        <div className="flex items-center gap-3">
-          {/* <Image
-            src="/skillexchange2.0.png"
-            alt="Logo"
-            width={48}
-            height={48}
-          /> */}
-          <h1 className="text-xl font-bold">SkillExchange</h1>
+        <div className="flex items-center gap-3 h-18 overflow-hidden">
+          <Link href="/protected">
+            <Image
+              src="/skillexchange2.0.png"
+              alt="Logo"
+              width={1024}
+              height={1024}
+              className="h-42 w-80 object-cover cursor-pointer mt-2" />
+          </Link>
+
         </div>
 
         {/* Right: User */}
@@ -30,7 +32,7 @@ export function Header({ username, avatarUrl }: HeaderProps) {
           className="flex items-center gap-3 hover:bg-gray-100 px-3 py-2 rounded-lg transition"
         >
           <span className="text-md font-medium text-gray-800">
-           <strong>{username}</strong>
+            <strong>{username}</strong>
           </span>
 
           <div className="w-10 h-10 rounded-full overflow-hidden bg-gray-200 flex items-center justify-center">
