1f7718e7da9a3eb225b8b3356b690a68a63298d7
RonjaSingh <ronja-maria.singh@dci-student.org>
2026-03-04 15:00:53 +0100

    style(Myposts): add styling

--- src/app/protected/advertisement/page.tsx
+++ src/app/protected/advertisement/page.tsx
@@ -47,15 +47,15 @@ export default function AdvertisementPage() {
   }
 
   return (
-    <div className="max-w-3xl mx-auto mt-10 p-4 space-y-10">
-      <h1 className="text-2xl text-center underline font-semibold text-gray-800">My Board Postings</h1>
+    <div className="max-w-7xl mx-auto m-0 p-4 space-y-10">
+      <h1 className="text-xl text-center font-semibold text-gray-800 bg-white/25 backdrop-blur-md rounded-2xl p-2">My Board Postings</h1>
 
       {/* Angebote */}
       <section className="bg-white/25 backdrop-blur rounded-2xl shadow-md p-6">
-        <h2 className="text-xl font-semibold underline mb-4">My Offers</h2>
+        <h2 className="text-lg text-center font-semibold text-gray-800 mb-4 backdrop-blur-md rounded-2xl p-2">My OFFERS</h2>
 
         {angebote.length === 0 ? (
-          <p className="text-sm text-gray-600">No offers available</p>
+          <p className="text-md text-gray-600">No offers available</p>
         ) : (
           <div className="space-y-4">
             {angebote.map((ad) => (
@@ -72,10 +72,10 @@ export default function AdvertisementPage() {
 
       {/* Gesuche */}
       <section className="bg-white/25 backdrop-blur rounded-2xl shadow-md p-6">
-        <h2 className="text-xl font-semibold underline mb-4">My Requests</h2>
+        <h2 className="text-lg text-center font-semibold text-gray-800 mb-4 backdrop-blur-md rounded-2xl p-2">MY REQUESTS</h2>
 
         {gesuche.length === 0 ? (
-          <p className="text-sm text-gray-600">No requests available</p>
+          <p className="text-md text-gray-600">No requests available</p>
         ) : (
           <div className="space-y-4">
             {gesuche.map((ad) => (
@@ -188,16 +188,14 @@ function Anzeige({
       <div className="flex gap-2">
         <button
           onClick={onEdit}
-          className="w-32 p-2 rounded-xl backdrop-blur-md text-gray-700 font-semibold text-md shadow-lg  hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0 active:shadow-md transition-all duration-200 ease-out
- hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0 active:shadow-md transition-all duration-200 ease-out"
+          className="w-36 p-2 rounded-xl bg-brand-mint/10 backdrop-blur-md text-gray-700 font-semibold text-md shadow-lg border border-brand-mint/40 hover:bg-brand-mint/60 hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0 active:shadow-md transition-all duration-200 ease-out"
         >
           Edit
         </button>
 
         <button
           onClick={() => onDelete(ad)}
-          className="w-32 p-2 rounded-xl backdrop-blur-md text-gray-700 font-semibold text-md shadow-lg  hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0 active:shadow-md transition-all duration-200 ease-out
- hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0 active:shadow-md transition-all duration-200 ease-out"
+          className="w-36 p-2 rounded-xl bg-brand-pink/10 backdrop-blur-md text-gray-700 font-semibold text-md shadow-lg border border-brand-pink/40 hover:bg-brand-pink/60 hover:border-brand-pink hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0 active:shadow-md transition-all duration-200 ease-out"
         >
           Delete
         </button>
