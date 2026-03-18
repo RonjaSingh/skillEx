a39ce40664462b922461cda46ae458dd29ca0d12
Raphael <raphael.lesmann@dci-student.org>
2026-03-18 12:52:45 +0100

    improve render speed in id-page.tsx

--- src/app/protected/profile/[id]/page.tsx
+++ src/app/protected/profile/[id]/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from "@/lib/supabase/server";
 import { User } from "lucide-react";
 import ReadMoreCard from "@/components/readmore";
 import PublicProfileCalendar from "@/components/public-profile-calendar";
-import StarRating from "@/components/average-rating";
+
 
 
 type UserProfile = {
@@ -34,13 +34,13 @@ export default async function ProfilePage({
 
   const supabase = await createClient();
 
-
+/*
   const { data: authData } = await supabase.auth.getUser()
   const currentUserId = authData.user?.id
   const isOwner = currentUserId === id
 
   /* User Profil laden */
-  const { data: user, error } = await supabase
+ /* const { data: user, error } = await supabase
     .from("user")
     .select(`
       id,
@@ -83,6 +83,64 @@ const averageRating =
 
   const skills =
     user.user_skills?.map((s) => s.skills.name) || [];
+    */
+
+    
+
+const [
+  /*{ data: authData },*/
+  { data: user, error },
+  { data: ratings }
+] = await Promise.all([
+  /*supabase.auth.getUser(),*/
+  supabase
+    .from("user")
+    .select(`
+      id,
+      name,
+      profile_image,
+      user_language (
+        language:language_id ( name )
+      ),
+      user_skills (
+        skills ( name )
+      )
+    `)
+    .eq("id", id)
+    .single<UserProfile>(),
+  supabase
+    .from('rating')
+    .select('stars')
+    .eq('reviewed_user_id', id)
+])
+
+// Auth
+/*const currentUserId = authData.user?.id
+const isOwner = currentUserId === id */
+
+// Error handling
+if (error || !user) {
+  return <div className="p-6">Profil nicht gefunden</div>
+}
+
+// Ratings
+const safeRatings = ratings ?? []
+
+const ratingCount = safeRatings.length
+
+const averageRating =
+  ratingCount > 0
+    ? safeRatings.reduce((sum, r) => sum + r.stars, 0) / ratingCount
+    : null
+
+// Mapping
+const languages =
+  user.user_language?.map((l) => l.language.name) || []
+
+const skills =
+  user.user_skills?.map((s) => s.skills.name) || []
+
+
 
   return (
     <div className="max-w-8xl mx-auto mt-4 px-24 space-y-12 text-gray-800 font-semibold">
