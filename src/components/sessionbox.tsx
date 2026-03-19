a80767143afa3c9ee368bf489297a088cde065f6
RonjaSingh <ronja-maria.singh@dci-student.org>
2026-03-19 10:24:56 +0100

    deployment trigger: also improve editprofile pop up styling

--- src/components/sessionbox.tsx
+++ src/components/sessionbox.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 export default function SessionBox({ title, children }: Props) {
   return (
-    <section className="bg-white/15 backdrop-blur rounded-2xl shadow-md p-6">
+    <section className="bg-white/15 backdrop-blur rounded-2xl shadow-md p-8">
 
-      <h2 className="text-lg text-center font-semibold text-gray-800 mb-6 backdrop-blur-md rounded-2xl p-2">
+      <h2 className="text-lg text-center font-semibold text-gray-800 mb-2 backdrop-blur-md rounded-2xl p-2">
         {title}
       </h2>
 
